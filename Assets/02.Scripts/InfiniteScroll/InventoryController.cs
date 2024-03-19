1392ffec6175cc3c999d62de7628541a9d39d1ef
Imnyeong <lmh4873@gmail.com>
2024-03-19 20:05:55 +0900

    [240319] - InfiniteScroll 커스텀

---
+++ Assets/02.Scripts/InfiniteScroll/InventoryController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Imnyeong
+{
+    [RequireComponent(typeof(InfiniteScroll))]
+    public class InventoryController : MonoBehaviour, IInfiniteScrollSetup
+    {
+		private int max;
+
+		public void OnPostSetupItems()
+		{
+			max = GameManager.instance.localDataBase.ingredientInventory.Count;
+			var infiniteScroll = GetComponent<InfiniteScroll>();
+			infiniteScroll.onUpdateItem.AddListener(OnUpdateItem);
+			GetComponentInParent<ScrollRect>().movementType = ScrollRect.MovementType.Clamped;
+
+			var rectTransform = GetComponent<RectTransform>();
+			var delta = rectTransform.sizeDelta;
+			delta.y = infiniteScroll.itemScale * max;
+			rectTransform.sizeDelta = delta;
+		}
+
+		public void OnUpdateItem(int itemCount, GameObject obj)
+		{
+			if (itemCount < 0 || itemCount >= max)
+			{
+				obj.SetActive(false);
+			}
+			else
+			{
+				obj.SetActive(true);
+				var item = obj.GetComponentInChildren<InventoryItem>();
+				item.UpdateItem(itemCount);
+			}
+		}
+	}
+}
